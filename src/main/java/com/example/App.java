e57a59329b9ceead2e34499fb823341ef598d7f8
jessyjyr <jessyyr02@gmail.com>
2024-05-28 12:41:40 +0200

    ADD SUFIJO F

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Mundo!" );
+        
+     // TIPO DE DATOS PRIMITIVO: 
+        // No tiene propiedades, ni metodos.
+        int a = 10;
+
+       // Los tipos de datos OBJETOS: 
+        // Son un envoltorio de los tipos primitivos. Si tienen propiedades y metodos.
+        // Por cada tipo primitivo existe un tipo objeto correspondiente. 
+
+       Integer b = 20;
+
+       float c = 2.3f;
+           
+        
     }
 }
